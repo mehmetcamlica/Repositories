8d7c1c51d9930570dc41e433b5e747b7c0ef6e26
Mehmet Camlica <98692913+mehmetcamlica@users.noreply.github.com>
2024-01-13 22:17:55 +0100

    Add files via upload

---
+++ day02_scanner_dataCasting_wrapperClasses/C03_Scanner.java
@@ -0,0 +1,22 @@
+package day02_scanner_dataCasting_wrapperClasses;
+
+import java.util.Scanner;
+
+public class C03_Scanner {
+
+    public static void main(String[] args) {
+
+        //Soru 4- Kullanicidan bir dikdortgenin 2 kenar uzunlugunu alip,
+        //        dikdortgenin alanini yazdirin.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen dikdortgenin kenar uzunluklarini giriniz");
+
+        double kenar1 = scanner.nextDouble();
+        double kenar2 = scanner.nextDouble();
+
+        System.out.println("Dikdortgenin alani : " + kenar2*kenar1);
+
+    }
+}

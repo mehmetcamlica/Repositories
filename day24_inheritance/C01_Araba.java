3185fa8419d949ba5850f43cbabec5b9dcf5c71a
Mehmet Camlica <98692913+mehmetcamlica@users.noreply.github.com>
2024-02-19 11:38:58 +0100

    Add files via upload

---
+++ day24_inheritance/C01_Araba.java
@@ -0,0 +1,11 @@
+package day24_inheritance;
+
+public class C01_Araba {
+    C01_Araba(){
+        System.out.println("Araba constructor calisti");
+    }
+
+    String marka = "Belirtilmedi";
+    String model = "Belirtilmedi";
+    String kasa = "Belirtilmedi";
+}
